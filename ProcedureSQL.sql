0c8848826b36ae94395d8684a68c24d170552493
Hanie20 <66524189+Hanie20@users.noreply.github.com>
2024-10-23 16:02:29 -0400

    Update ProcedureSQL.sql

--- ProcedureSQL.sql
+++ ProcedureSQL.sql
@@ -1,18 +1,18 @@
 USE AdventureWorksDW2022  
 GO
 
--- VÈrifie si la procÈdure existe dÈj‡ et la supprime si c'est le cas
+-- V√©rifie si la proc√©dure existe d√©j√† et la supprime si c'est le cas
 IF OBJECT_ID('dbo.UpdateFrenchProductCategoryName', 'P') IS NOT NULL
     DROP PROCEDURE dbo.UpdateFrenchProductCategoryName;
 GO
 
--- CrÈe la nouvelle procÈdure stockÈe
+-- Cr√©e la nouvelle proc√©dure stock√©e
 CREATE PROCEDURE dbo.UpdateFrenchProductCategoryName
     @NewName NVARCHAR(50),
     @OldName NVARCHAR(50)
 AS
 BEGIN
-    -- Met ‡ jour le nom de la catÈgorie de produit en franÁais
+    -- Met √† jour le nom de la cat√©gorie de produit en fran√ßais
     UPDATE dbo.DimProductSubcategory
     SET dbo.DimProductSubcategory.FrenchProductSubcategoryName = @NewName
     WHERE dbo.DimProductSubcategory.FrenchProductSubcategoryName = @OldName
@@ -21,16 +21,16 @@ END;
 GO
 
 
--- Liste toutes les procÈdures stockÈes dans la base de donnÈes
+-- Liste toutes les proc√©dures stock√©es dans la base de donn√©es
 SELECT * FROM sys.procedures WHERE name = 'UpdateFrenchProductCategoryName';
 
 USE AdventureWorksDW2022;
 GO
 
--- Appelle la procÈdure
+-- Appelle la proc√©dure
 EXEC dbo.UpdateFrenchProductCategoryName @NewName = 'VTT', @OldName = 'test';
 
--- Affiche le texte de la procÈdure existante
+-- Affiche le texte de la proc√©dure existante
 EXEC sp_helptext 'dbo.UpdateFrenchProductCategoryName';
 
 
@@ -41,24 +41,34 @@ GO
 ALTER PROCEDURE dbo.UpdateFrenchProductCategoryName
     @NewName NVARCHAR(50),
     @OldName NVARCHAR(50),
-    @NewEnglishName NVARCHAR(50) = NULL  -- Nouveau paramËtre avec valeur par dÈfaut
+    @NewEnglishName NVARCHAR(50) = NULL  -- Nouveau param√®tre avec valeur par d√©faut
 AS
 BEGIN
-    -- Met ‡ jour le nom de la catÈgorie de produit en franÁais
+    -- Met √† jour le nom de la cat√©gorie de produit en fran√ßais
 	UPDATE dbo.DimProductSubcategory
     SET 
 	dbo.DimProductSubcategory.FrenchProductSubcategoryName = @NewName,
-	dbo.DimProductSubcategory.EnglishProductSubcategoryName = COALESCE(@NewEnglishName, dbo.DimProductSubcategory.EnglishProductSubcategoryName)  -- Met ‡ jour l'anglais si fourni
+	dbo.DimProductSubcategory.EnglishProductSubcategoryName = COALESCE(@NewEnglishName, dbo.DimProductSubcategory.EnglishProductSubcategoryName)  -- Met √† jour l'anglais si fourni
     WHERE dbo.DimProductSubcategory.FrenchProductSubcategoryName = @OldName
 	AND dbo.DimProductSubcategory.ProductCategoryKey = 1
 END;
 GO
 
--- Appel avec le nouveau paramËtre
+-- Appel avec le nouveau param√®tre
 EXEC dbo.UpdateFrenchProductCategoryName @NewName = 'test', @OldName = 'VTT', @NewEnglishName = 'Mountain Bike';
 
--- Appel sans le nouveau paramËtre
+-- Appel sans le nouveau param√®tre
 EXEC dbo.UpdateFrenchProductCategoryName @NewName = 'VTT', @OldName = 'test';
+
+
+USE StudentPortalDB
+Go
+CREATE PROCEDURE GetStudents
+AS
+BEGIN
+    SELECT * FROM dbo.Students
+‚ÄÇ‚ÄÇ‚ÄÇ‚ÄÇ‚ÄÇ‚ÄÇWHERE dbo.Students.Name LIKE 'Ja%'
+END
 
 
 
